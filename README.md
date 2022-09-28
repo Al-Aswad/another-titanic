bd19142835600772ac6a26e072a4abbc95e7da3d
Al-Aswad <73427522+Al-Aswad@users.noreply.github.com>
2022-09-28 18:23:42 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# another-titanic
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-kymvs6)
---
+++ index.js
@@ -0,0 +1,27 @@
+import persons from './persons.json';
+
+console.log('Person', persons);
+
+//sort by Age
+persons.sort((a, b) => {
+  let x = a.age;
+  let y = b.age;
+  if (x < y) {
+    return -1;
+  }
+  if (x > y) {
+    return 1;
+  }
+  return 0;
+});
+
+//function group person by age
+const groupPersonsByAge = persons.reduce((group, p, index) => {
+  const { age } = p;
+  group[age] = group[age] ?? [];
+  group[age].push(p.name);
+  return group;
+}, {});
+
+console.log('Person after Sort ', persons);
+console.log('Person Group ', groupPersonsByAge);
